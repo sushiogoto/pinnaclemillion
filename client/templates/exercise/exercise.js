2a059b15258f2ab207859016cf6f647e7e172064
sushiogoto <yoshio.goto@gmail.com>
2016-03-16 14:49:07 +0000

    Add collection methods and buttons for 75kg and 100kg sets, and update weight display method on exercise page

--- client/templates/exercise/exercise.js
+++ client/templates/exercise/exercise.js
@@ -1,15 +1,25 @@
 Session.setDefault('counter', 0);
 
 Template.exercise.helpers({
-  counter: function () {
+  totalWeightMoved: function () {
     return Exercises.find().fetch()[0].totalWeightMoved;
   }
 });
 
 Template.exercise.events({
-  'click button': function () {
+  'click .50-kilos': function () {
     // increment the counter when button is clicked
     // Session.set('counter', Session.get('counter') + 1);
     Meteor.call('addFiftyKiloSet', function (error, result) {});
-  }
+  },
+  'click .75-kilos': function () {
+    // increment the counter when button is clicked
+    // Session.set('counter', Session.get('counter') + 1);
+    Meteor.call('addSeventyFiveKiloSet', function (error, result) {});
+  },
+  'click .100-kilos': function () {
+    // increment the counter when button is clicked
+    // Session.set('counter', Session.get('counter') + 1);
+    Meteor.call('addOneHundredKiloSet', function (error, result) {});
+  },
 });
