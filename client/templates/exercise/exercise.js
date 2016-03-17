fbb37bf4234a91d97a920bbaf9ce86788ed98660
sushiogoto <yoshio.goto@gmail.com>
2016-03-17 13:22:54 -0700

    Clean up extraneous commented out code and extra files

--- client/templates/exercise/exercise.js
+++ client/templates/exercise/exercise.js
@@ -6,19 +6,12 @@ Template.exercise.helpers({
 
 Template.exercise.events({
   'click #50-kilos': function () {
-    // increment the counter when button is clicked
-    // Session.set('counter', Session.get('counter') + 1);
     Meteor.call('addFiftyKiloSet', function (error, result) {});
-    // $('#50-kilos').value
   },
   'click #75-kilos': function () {
-    // increment the counter when button is clicked
-    // Session.set('counter', Session.get('counter') + 1);
     Meteor.call('addSeventyFiveKiloSet', function (error, result) {});
   },
   'click #100-kilos': function () {
-    // increment the counter when button is clicked
-    // Session.set('counter', Session.get('counter') + 1);
     Meteor.call('addOneHundredKiloSet', function (error, result) {});
   },
 });
