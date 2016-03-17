818a5d397bd615088fde9783064e3bb5e03c36f6
sushiogoto <yoshio.goto@gmail.com>
2016-03-16 22:52:16 -0700

    Add responsive landing page weight moved font size

--- server/accounts.js
+++ server/accounts.js
@@ -25,14 +25,3 @@ Meteor.startup(function() {
     });
   }
 })
-
-Accounts.validateNewUser(function (user) {
-  var loggedInUser = Meteor.user();
-
-  if (Roles.userIsInRole(loggedInUser, ['admin','manage-users'])) {
-    // NOTE: This example assumes the user is not using groups.
-    return true;
-  }
-
-  throw new Meteor.Error(403, "Not authorized to create new users");
-});
